d08c6e11c151bd0be9c8c197810f0ad7234b8aeb
Josh Manders <josh@joshmanders.com>
2016-06-30 09:47:39 -0500

    add support for defining individual seeds to run

--- core/commands/db-seed.js
+++ core/commands/db-seed.js
@@ -1,11 +1,13 @@
 import { basename } from 'path'
 import { db } from 'core/bookshelf'
 
-export const command = 'db:seed'
+export const command = 'db:seed [files...]'
 export const description = 'seed the database with records'
 
-export const action = (done) => () => {
-  db.seed.run().spread((seeds) => {
+export const action = (done) => (files) => {
+  const seed = files.length > 0 ? db.seed._runSeeds(files) : db.seed.run()
+
+  seed.spread((seeds) => {
     if (seeds.length === 0) {
       console.log('No seed files exist')
     } else {
