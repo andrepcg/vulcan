5a3595be955a91e55442dd4b5b4871d9967ed5df
Josh Manders <josh@joshmanders.com>
2016-07-05 11:54:40 -0500

    add seed flag to reset

--- core/commands/db-reset.js
+++ core/commands/db-reset.js
@@ -3,8 +3,12 @@ import { db } from 'core/bookshelf'
 
 export const command = 'db:reset'
 export const description = 'reset database and run migrations'
+export const options = [{
+  flags: '-S, --seed',
+  description: 'Run seeds after reset'
+}]
 
-export const action = (done) => () => {
+export const action = (done) => ({ seed = false }) => {
   db.migrate.forceFreeMigrationsLock()
   .then(() => db.migrate._listCompleted())
   .then((completed) => db.migrate._waterfallBatch(0, completed.reverse(), 'down'))
@@ -16,6 +20,17 @@ export const action = (done) => () => {
       console.log(`Batch ${batch} ran ${migrations.length} migrations:`)
       migrations.map((file) => console.log(basename(file, '.js')))
     }
-    done()
+    if (seed) {
+      return db.seed.run().spread((seeds) => {
+        console.log('\n---\n')
+        if (seeds.length === 0) {
+          console.log('No seed files exist')
+        } else {
+          console.log(`Ran ${seeds.length} seed files:`)
+          seeds.map((file) => console.log(basename(file, '.js')))
+        }
+      })
+    }
   })
+  .then(() => done())
 }
