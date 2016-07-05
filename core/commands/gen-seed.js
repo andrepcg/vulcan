589e463dda28dd217adafc4b2d9adc15419da25f
Josh Manders <josh@joshmanders.com>
2016-07-05 13:01:49 -0500

    remove redundant seed code

--- core/commands/gen-seed.js
+++ core/commands/gen-seed.js
@@ -6,17 +6,14 @@ import { pluralize } from 'inflection'
 export const command = 'gen:seed <model>'
 export const description = 'create a new seed file'
 
-const template = (Model) => `import Promise from 'bluebird'
-import faker from 'faker'
+const template = (Model) => `import faker from 'faker'
 import ${upperFirst(Model)} from 'app/models/${Model.toLowerCase()}'
 
-export const seed = (db) => Promise.all([
-  ${Model}.create([{
-    field: 'value_2'
-  }, {
-    field: 'value_1'
-  }])
-])
+export const seed = () => ${Model}.create([{
+  field: 'value_2'
+}, {
+  field: 'value_1'
+}])
 `
 
 export const action = (done) => (model, { table }) => {
